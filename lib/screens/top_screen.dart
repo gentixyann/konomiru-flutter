083de66157e013e2f7fa10975c6e24128ce19187
Gento <a551.return@gmail.com>
2021-03-31 00:03:24 +0900

    movie cardを分割

--- lib/screens/top_screen.dart
+++ lib/screens/top_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/movie_card.dart';
+
 class TopScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,54 +13,7 @@ class TopScreen extends StatelessWidget {
         padding: EdgeInsets.all(20),
         child: Column(
           children: <Widget>[
-            Card(
-              child: Row(
-                children: <Widget>[
-                  Image.network(
-                      'https://image.tmdb.org/t/p/w154//5KYaB1CTAklQHm846mHTFkozgDN.jpg'),
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            width: double.infinity,
-                            margin: EdgeInsets.only(bottom: 15),
-                            // height: 50,
-                            child: Text(
-                              'Godzilla vs. Kong',
-                              style: Theme.of(context).textTheme.headline6,
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                          Container(
-                            width: double.infinity,
-                            margin: EdgeInsets.only(bottom: 15),
-                            // height: 20,
-                            child: Text(
-                              '2021-03-24',
-                              style: TextStyle(
-                                color: Colors.grey.shade700,
-                              ),
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                          Container(
-                            width: double.infinity,
-                            // height: 60,
-                            child: Text(
-                              'In a time when monsters walk the Earth, humanity’s...',
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            )
+            MovieCard(),
           ],
         ),
       ),
---
+++ lib/widgets/movie_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MovieCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: <Widget>[
+          Image.network(
+              'https://image.tmdb.org/t/p/w154//5KYaB1CTAklQHm846mHTFkozgDN.jpg'),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(bottom: 15),
+                    // height: 50,
+                    child: Text(
+                      'Godzilla vs. Kong',
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(bottom: 15),
+                    // height: 20,
+                    child: Text(
+                      '2021-03-24',
+                      style: TextStyle(
+                        color: Colors.grey.shade700,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    // height: 60,
+                    child: Text(
+                      'In a time when monsters walk the Earth, humanity’s...',
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
