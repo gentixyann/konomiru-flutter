bb39970b614bafc4842accc92431f31ec22c42be
Gento <a551.return@gmail.com>
2021-05-01 11:35:47 +0900

    search_movie_detail_screenの原型を作成

--- lib/screens/search_movie_detail_screen.dart
+++ lib/screens/search_movie_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/add_movie_button.dart';
+import '../widgets/space_box.dart';
 
 class SearchMovieDetailScreen extends StatelessWidget {
   static const routeName = '/search-movie-detail';
@@ -10,6 +11,7 @@ class SearchMovieDetailScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, Object>;
     final selectedMovie = routeArgs;
     final backdropPath = selectedMovie['backdropPath'];
+    final posterPath = selectedMovie['posterPath'];
 
     return Scaffold(
       body: CustomScrollView(
@@ -49,27 +51,41 @@ class SearchMovieDetailScreen extends StatelessWidget {
                 Container(
                   padding: EdgeInsets.all(20),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(bottom: 15),
-                        child: Text(
-                          'Release Date',
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.network(
+                            'https://image.tmdb.org/t/p/w154/${posterPath}',
+                          ),
+                        ],
                       ),
-                      Text(selectedMovie['releaseDate']),
-                      Container(
-                        margin: EdgeInsets.only(bottom: 15),
-                        child: Text(
-                          'Overview',
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                      ),
-                      Text(selectedMovie['overview']),
-                      Container(
-                        child: Text(selectedMovie['id'].toString()),
-                      ),
+                      SpaceBox.height(20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(bottom: 15),
+                            child: Text(
+                              'Release Date',
+                              style: Theme.of(context).textTheme.headline5,
+                            ),
+                          ),
+                          Text(selectedMovie['releaseDate']),
+                          SpaceBox.height(20),
+                          Container(
+                            margin: EdgeInsets.only(bottom: 15),
+                            child: Text(
+                              'Overview',
+                              style: Theme.of(context).textTheme.headline5,
+                            ),
+                          ),
+                          Text(selectedMovie['overview']),
+                          Container(
+                            child: Text(selectedMovie['id'].toString()),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 )
