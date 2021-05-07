a61931433870ace181ef2535096082b7a1f8d51d
Gento <a551.return@gmail.com>
2021-05-07 21:11:30 +0900

    auth_formの原型作成

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../widgets/auth/auth_form.dart';
 
 class AuthScreen extends StatefulWidget {
   AuthScreen({Key key}) : super(key: key);
@@ -8,6 +12,9 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  var _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Container();
